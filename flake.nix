053d997e2dba4c14f7d6021fb4ddedcc72147926
digitaldive <digitaldive@Peters-MBP.attlocal.net>
2024-09-02 13:46:16 -0700

    --no-edit

--- flake.nix
+++ flake.nix
@@ -1,16 +1,30 @@
 # in flake.nix
 {
   inputs = {
-    # this is equivalent to `nixpkgs = { url = "..."; };`
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # 👇 we have a new input!
+    flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = inputs: {
-    packages."x86_64-linux".default = derivation {
-      name = "simple";
-      builder = "${inputs.nixpkgs.legacyPackages."x86_64-linux".bash}/bin/bash";
-      args = [ "-c" "echo foo > $out" ];
-      src = ./.;
-      system = "x86_64-linux";
-    };
-  };
+  #      it's destructured here 👇
+  outputs = { self, nixpkgs, flake-utils }:
+    # calling a function from `flake-utils` that takes a lambda
+    # that takes the system we're targetting
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        # no need to define `system` anymore
+        name = "simple";
+        src = ./.;
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        # `eachDefaultSystem` transforms the input, our output set
+        # now simply has `packages.default` which gets turned into
+        # `packages.${system}.default` (for each system)
+        packages.default = derivation {
+          inherit system name src;
+          builder = with pkgs; "${bash}/bin/bash";
+          args = [ "-c" "echo foo > $out" ];
+        };
+      }
+    );
 }
